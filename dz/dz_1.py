ec6c1d964414c44acae4852f02caa0de1a08635d
eldar2025 <eldarnagiev94@gmail.com>
2025-04-06 22:24:58 +0300

    added print

--- dz/dz_1.py
+++ dz/dz_1.py
@@ -259,14 +259,19 @@
 
 # 20.03.2025
 
-def negative_number(n):
-    if not n:
-        return 0
-    count = 0
-    if n[0] < 0:
-        count += 1
-    return count + negative_number(n[1:])
+# def negative_number(n):
+#     if not n:
+#         return 0
+#     count = 0
+#     if n[0] < 0:
+#         count += 1
+#     return count + negative_number(n[1:])
+#
+#
+# lst = [-2, 3, 8, -11, -4, 6]
+# print(negative_number(lst))
+#
+#
+#
 
-
-lst = [-2, 3, 8, -11, -4, 6]
-print(negative_number(lst))
+print("Я сделал это домашнее задание")

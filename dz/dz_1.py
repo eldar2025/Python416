be6e9d8dd59350f8b58089f859c07008f35eebd0
eldar2025 <eldarnagiev94@gmail.com>
2025-04-11 16:26:47 +0300

    new3

--- dz/dz_1.py
+++ dz/dz_1.py
@@ -277,24 +277,36 @@
 # print("Я сделал это домашнее задание")
 
 
-f = open("test3.txt", "w")
-f.write("Замена строки в текстовом файле;\nизменить строку в списке;\nзаписать список в файл")
-f.close()
+# f = open("test3.txt", "w")
+# f.write("Замена строки в текстовом файле;\nизменить строку в списке;\nзаписать список в файл")
+# f.close()
+#
+# f = open("test3.txt", "r")
+# read_line = f.readlines()
+# print(read_line)
+# f.close()
+#
+# pos1 = int(input("Pos1 = "))
+# pos2 = int(input("Pos2 = "))
+# if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):
+#     read_line[pos1], read_line[pos2] = read_line[pos2], read_line[pos1]
+# else:
+#     print("Такой строки нет")
+#
+# print(read_line)
+#
+# f = open("test3.txt", "w")
+# f.writelines(read_line)
+# f.close()
 
-f = open("test3.txt", "r")
-read_line = f.readlines()
-print(read_line)
-f.close()
 
-pos1 = int(input("Pos1 = "))
-pos2 = int(input("Pos2 = "))
-if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):
-    read_line[pos1], read_line[pos2] = read_line[pos2], read_line[pos1]
+#03.04.2025
+import os
+file_path = r"C:\Users\User\Desktop\Python\dz\test3.txt"
+
+if os.path.exists(file_path):
+    directory, name = os.path.split(file_path)
+    atime = os.path.getatime(file_path)
+    print(f"{name} ({directory})- время последнего доступа к файлу {atime} секунд ")
 else:
-    print("Такой строки нет")
-
-print(read_line)
-
-f = open("test3.txt", "w")
-f.writelines(read_line)
-f.close()
+    print(f"Файл {file_path} не существует")
